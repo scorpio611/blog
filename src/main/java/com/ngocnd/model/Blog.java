cccc5f88355fb890141307c8e3026ae1a9c0e2ec
scorpio611 <evilmonter666@gmail.com>
2019-01-18 11:41:20 +0700

    Blog

--- src/main/java/com/ngocnd/model/Blog.java
+++ src/main/java/com/ngocnd/model/Blog.java
@@ -1,8 +1,9 @@
 package com.ngocnd.model;
 
-import com.sun.istack.internal.NotNull;
+
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "blogs")
